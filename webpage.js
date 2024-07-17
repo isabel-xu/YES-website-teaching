5d9bd1f3a8eac7b2be48fba31dac1d06d961d70f
isabel-xu <isabel.xu47@gmail.com>
2024-07-17 14:21:08 -0500

    Update webpage.js

--- webpage.js
+++ webpage.js
@@ -1,45 +1,18 @@
-var layout = {
-  title: {
-    text:'Plot Title',
-    font: {
-      family: 'Courier New, monospace',
-      size: 24
-    },
-    xref: 'paper',
-    x: 0.05,
-  },
-  paper_bgcolor: "orange",
-  xaxis: {
-    title: {
-      text: 'x Axis Line',
-      font: {
-        family: 'Courier New, monospace',
-        size: 18,
-        color: '#7f7f7f'
-      }
-    },
-  },
-  yaxis: {
-    title: {
-      text: 'y Axis Line',
-      font: {
-        family: 'Courier New, monospace',
-        size: 18,
-        color: '#7f7f7f'
-      }
-    }
-  }
-};
+<head>
+               <script src="https://cdn.plot.ly/plotly-2.32.0.min.js" charset="utf-8"></script>
+</head>
 
-TESTER = document.getElementById("tester");
-Plotly.newPlot(
-  TESTER,
-  [
-    {
-      x: ['red', 'green', 'orange', 'yellow', 'purple'],
-      y: [1, 2, 4, 8, 16],
-    type: 'bar'
-    }
-  ],
-  layout
-);
+<head>
+  <script src="plotly-2.32.0.min.js" charset="utf-8"></script>
+</head>
+
+<div id="tester" style="width:600px;height:250px;"></div>
+
+<script>
+  TESTER = document.getElementById('tester');
+  Plotly.newPlot( TESTER, [{
+  x: [470, 525, 560, 585, 600, 645, 700, 735, 810, 880, 940],
+  y: [-0.3273322422, 7.395498392, 18.53035144, 66.28289474, 88.6914378, 106.539075, 115.5594406, 122.761194, 120.8333333, 119.4762684, 119.1204589] }], {
+  margin: { t: 0 } } );
+Plotly.newPlot('myDiv', data, layout);
+</script>
