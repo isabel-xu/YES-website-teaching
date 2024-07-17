f43f02759f301f0588878f6db6a2609fd1617ae2
isabel-xu <isabel.xu47@gmail.com>
2024-07-17 14:36:19 -0500

    Update webpage.js

--- webpage.js
+++ webpage.js
@@ -1,15 +1,6 @@
-<head>
-               <script src="https://cdn.plot.ly/plotly-2.32.0.min.js" charset="utf-8"></script>
-</head>
 
-
-<div id="tester" style="width:600px;height:250px;"></div>
-
-<script>
   TESTER = document.getElementById('tester');
   Plotly.newPlot( TESTER, [{
   x: [470, 525, 560, 585, 600, 645, 700, 735, 810, 880, 940],
   y: [-0.3273322422, 7.395498392, 18.53035144, 66.28289474, 88.6914378, 106.539075, 115.5594406, 122.761194, 120.8333333, 119.4762684, 119.1204589] }], {
   margin: { t: 0 } } );
-
-</script>
